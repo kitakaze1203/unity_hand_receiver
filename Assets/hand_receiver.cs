bb34deec2720ae6025db181757d781b859dddacf
dkitakazeb <kantaorange@gmail.com>
2026-03-23 12:10:17 +0900

    2026/03/23

--- Assets/hand_receiver.cs
+++ Assets/hand_receiver.cs
@@ -5,6 +5,7 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading;
+using TMPro;
 
 public class hand_receiver : MonoBehaviour
 {
@@ -16,7 +17,8 @@ public class hand_receiver : MonoBehaviour
     Vector3 euler = new Vector3();
     Vector3 cor_euler = new Vector3();
     [SerializeField] private string send_IP;
-
+    [SerializeField] TextMeshProUGUI state;
+    private string message;
     // Start is called before the first frame update
     void Start()
     {
@@ -30,6 +32,7 @@ public class hand_receiver : MonoBehaviour
     void Update()
     {
         transform.eulerAngles = euler;
+        state.text = message;
     }
 
     private void ReceiveData()
@@ -43,48 +46,40 @@ public class hand_receiver : MonoBehaviour
                 string text = Encoding.UTF8.GetString(data);
 
                 string[] split = text.Split(',');
-                if (split.Length == 9)
+                if (split.Length == 10)
                 {
                     euler = new Quaternion(float.Parse(split[0]), float.Parse(split[1]), float.Parse(split[2]), float.Parse(split[3])).eulerAngles;
                     cor_euler = euler;
-                    if (cor_euler.x < 360 && cor_euler.x > 180)
-                    {
-                        cor_euler.x = cor_euler.x - 360;
-                    }
-                    if (cor_euler.y < 360 && cor_euler.y > 180)
-                    {
-                        cor_euler.y = cor_euler.y - 360;
-                    }
-                    if (cor_euler.z < 360 && cor_euler.z > 180)
-                    {
-                        cor_euler.z = cor_euler.z - 360;
-                    }
-                    int power_x = (int)cor_euler.x * 256 / 90;
-                    int power_y = (int)cor_euler.z * 256 / 90;
-                    int power_z = (int)cor_euler.y * 256 / 90;
-                    string message = $"{power_x},{power_y},{power_z},{split[4]},{split[5]},{split[6]},{split[7]},{split[8]}";
-                    Debug.Log(message);
+
+                    if (cor_euler.x < 360 && cor_euler.x > 180) cor_euler.x = cor_euler.x - 360;
+                    if (cor_euler.y < 360 && cor_euler.y > 180) cor_euler.y = cor_euler.y - 360;
+                    if (cor_euler.z < 360 && cor_euler.z > 180) cor_euler.z = cor_euler.z - 360;
+                    
+                    int power_x = (int)(cor_euler.x * 256 / 90);
+                    int power_y = (int)(cor_euler.z * 256 / 90);
+                    int power_z = (int)(cor_euler.y * 256 / 90);
+
+                    power_x = deadzone(power_x);
+                    power_y = deadzone(power_y);
+                    power_z = deadzone(power_z);
+
+                    message = $"{power_x},{power_y},{power_z},{split[4]},{split[5]},{split[6]},{split[7]},{split[8]},{split[9]}";
                     byte[] send_data = Encoding.UTF8.GetBytes(message);
-                    try
-                    {
-                        udpClient.Send(send_data, send_data.Length, send_IP, port);
-                    }
-                    catch { Debug.Log("senderror"); }
+                    
+                    try { udpClient.Send(send_data, send_data.Length, send_IP, port); }
+                    catch { state.text = "senderror"; }   
                 }
             }
             catch (System.Threading.ThreadAbortException)
             {
-                // スレッド破棄時の例外なので何もしない
             }
             catch (System.Net.Sockets.SocketException)
             {
-                // udpClient.Close() 時に発生するので、停止中なら無視する
-                if (running) Debug.LogWarning("Socket Error occurred.");
+                if (running) message = "Socket Error occurred.";
             }
             catch (System.Exception e)
             {
-                // それ以外の本当のエラーだけログに出す
-                if (running) Debug.LogWarning(e);
+                if (running) message = $"{e}";
             }
         }
     }
@@ -99,4 +94,11 @@ public class hand_receiver : MonoBehaviour
         }
     }
 
+    int deadzone(int data)
+    {
+        if (data > 255) return 255;
+        if (data < -255) return 255;
+        if (data < 25 && data > -25) return 0;
+        return data;
+    }
 }
